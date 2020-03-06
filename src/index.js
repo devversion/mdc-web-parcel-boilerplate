f8f9153b9c4f5e04951cc6a92a87791af824f699
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-06 17:09:09 +0100

    Quick logic to toggle density scale per button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import {MDCTextField} from '@material/textfield';
 
+const elements = [];
+
 document.querySelectorAll('.mdc-text-field').forEach(el => {
  new MDCTextField(el);
+ elements.push(el);
 });
+
+let current = 0;
+const AMOUNT_LEVELS = 4;
+
+global.toggle = () => {
+    elements.forEach(el => el.classList.remove('density-' + current));
+    current = (current + 1) % AMOUNT_LEVELS;
+    elements.forEach(el => el.classList.add('density-' + current));
+    updateLevel();
+}
+
+updateLevel();
+
+function updateLevel() {
+    document.querySelector('#current').textContent = `-${current}`;
+}
