d50f957087a2afbd51b28ae06af79aecd28d8a1d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-23 15:52:07 +0100

    Play with density for list

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ document.querySelectorAll('.mdc-text-field').forEach(el => {
  elements.push(el);
 });
 
+document.querySelectorAll('.mdc-list').forEach(el => {
+ elements.push(el);
+});
+
 let current = 0;
 const AMOUNT_LEVELS = 4;
 
 global.toggle = () => {
     elements.forEach(el => el.classList.remove('density-' + current));
-    current = (current + 1) % AMOUNT_LEVELS;
+    current = (current - 1) % AMOUNT_LEVELS;
     elements.forEach(el => el.classList.add('density-' + current));
     updateLevel();
 }
