64fd59518b1cfb227a2dc2ec5c501a1ddedc6839
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-10-26 16:46:57 +0200

    update boilerplate

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import {MDCList, MdcList} from '@material/list';
+import {MDCList} from '@material/list';
 
 const elements = [];
-
-document.querySelectorAll('.mdc-text-field').forEach(el => {
- //new MDCTextField(el);
- elements.push(el);
-});
 
 document.querySelectorAll('.mdc-list').forEach(el => {
 const list = new MDCList(el);
@@ -14,19 +9,3 @@ if (el.hasAttribute('single')) {
 }
  elements.push(el);
 });
-
-let current = 0;
-const AMOUNT_LEVELS = 4;
-
-global.toggle = () => {
-    elements.forEach(el => el.classList.remove('density-' + current));
-    current = (current - 1) % AMOUNT_LEVELS;
-    elements.forEach(el => el.classList.add('density-' + current));
-    updateLevel();
-}
-
-updateLevel();
-
-function updateLevel() {
-    document.querySelector('#current').textContent = `-${current}`;
-}
